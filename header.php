3d99d804bd02dd9be7ebd76ef68c2c22dbb150fe
Penknife92 <112814704+BruskiPawel@users.noreply.github.com>
2023-09-13 20:14:42 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<nav>
+    <div class="wrapper">
+        <ul>
+            <li><a href="index.php">Home</a> </li>
+            <li><a href="discovery.php">About Us</a> </li>
+            <li><a href="blog.php">Find Blogs</a> </li>
+            <li><a href="singup.php">Sing up</a> </li>
+            <li><a href="login.php">Log in</a> </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="wrapper">
